858982573319f8cb16ddd2217b02295df94f90bf
NavidJalali <navid.jalali@moia.io>
2024-11-30 04:36:15 +0100

    smoll cleanup

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
-ThisBuild / scalaVersion     := "2.13.12"
-ThisBuild / version          := "0.1.0-SNAPSHOT"
-ThisBuild / organization     := "com.CodeCrafters"
+ThisBuild / scalaVersion := "3.6.1"
+ThisBuild / version := "0.1.0-SNAPSHOT"
+ThisBuild / organization := "com.CodeCrafters"
 ThisBuild / organizationName := "CodeCrafters"
 
 assembly / assemblyJarName := "redis.jar"
@@ -10,5 +10,12 @@ lazy val root = (project in file("."))
     name := "codecrafter-redis",
     // List your dependencies here
     libraryDependencies ++= Seq(
-    )
+      "org.typelevel" %% "cats-core" % "2.12.0",
+      "org.typelevel" %% "cats-effect" % "3.5.7"
+    ),
+    assembly / assemblyJarName := "redis.jar",
+    assembly / assemblyMergeStrategy := {
+      case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+      case x                             => MergeStrategy.first
+    }
   )
--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -1,15 +1,74 @@
 package codecrafters_redis
 
-import java.net.{InetSocketAddress, ServerSocket}
+import cats.syntax.all.*
+import java.net.{ServerSocket}
+import codecrafters_redis.network.*
+import java.io.IOException
+import scala.util.Using.Manager
+import scala.util.Using.Releasable
+import scala.util.Using
+
+object network {
+  opaque type Port = Int
+  object Port {
+    def apply(value: Int): Port = value
+    extension (port: Port) {
+      def value: Int = port
+    }
+  }
+
+  opaque type Host = String
+  object Host {
+    def apply(value: String): Host = value
+    extension (host: Host) {
+      def value: String = host
+    }
+  }
+
+  final case class InetSocketAddress(host: Host, port: Port) {
+    import Port.*, Host.*
+    def asJava: java.net.InetSocketAddress =
+      new java.net.InetSocketAddress(host.value, port.value)
+  }
+
+  enum BindError {
+    case AlreadyBound(e: IOException)
+    case SecurityError(e: SecurityException)
+  }
+
+  given Releasable[ServerSocket] with
+    def release(resource: ServerSocket): Unit = resource.close()
+}
+
+private def bind(address: InetSocketAddress) =
+  val serverSocket = ServerSocket()
+  try
+    serverSocket.bind(address.asJava)
+    Right(serverSocket)
+  catch
+    case e: IOException =>
+      serverSocket.close()
+      Left(BindError.AlreadyBound(e))
+    case e: SecurityException =>
+      serverSocket.close()
+      Left(BindError.SecurityError(e))
 
 object Server {
   def main(args: Array[String]): Unit = {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println("Logs from your program will appear here!")
-    val serverSocket = new ServerSocket()
-    serverSocket.bind(new InetSocketAddress("localhost", 6379))
-    val clientSocket = serverSocket.accept() // wait for client
-    clientSocket.close()
-    serverSocket.close()
+    val address = InetSocketAddress(Host("localhost"), Port(6379))
+    bind(address) match
+      case Left(error) =>
+        Console.err.println(s"Failed to bind to $address: $error")
+        System.exit(1)
+      case Right(value) =>
+        println(s"Successfully bound to $address")
+        Using.resource(value) { serverSocket =>
+          println(s"Server is running on ${serverSocket.getLocalSocketAddress}")
+
+          val socket = serverSocket.accept()
+
+          println(s"Accepted connection from ${socket.getInetAddress}")
+
+        }
   }
 }
