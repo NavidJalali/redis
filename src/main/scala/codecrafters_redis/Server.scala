e105670b575a33fe35e83f8a5745edb80f6bee47
NavidJalali <navid.jalali@moia.io>
2024-11-30 13:27:39 +0100

    back to 2.13 i guess

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -1,62 +1,57 @@
 package codecrafters_redis
 
-import cats.syntax.all.*
+import cats.syntax.all._
 import java.net.{ServerSocket}
-import codecrafters_redis.network.*
+import codecrafters_redis.network._
 import java.io.IOException
 import scala.util.Using.Manager
 import scala.util.Using.Releasable
 import scala.util.Using
 
 object network {
-  opaque type Port = Int
+  type Port = Int
   object Port {
     def apply(value: Int): Port = value
-    extension (port: Port) {
-      def value: Int = port
-    }
   }
 
-  opaque type Host = String
+  type Host = String
   object Host {
     def apply(value: String): Host = value
-    extension (host: Host) {
-      def value: String = host
-    }
   }
 
   final case class InetSocketAddress(host: Host, port: Port) {
-    import Port.*, Host.*
     def asJava: java.net.InetSocketAddress =
-      new java.net.InetSocketAddress(host.value, port.value)
+      new java.net.InetSocketAddress(host, port)
   }
 
-  enum BindError {
-    case AlreadyBound(e: IOException)
-    case SecurityError(e: SecurityException)
+  sealed trait BindError extends Product with Serializable
+  object BindError {
+    final case class AlreadyBound(e: IOException) extends BindError
+    final case class SecurityError(e: SecurityException) extends BindError
   }
 
-  given Releasable[ServerSocket] with
-    def release(resource: ServerSocket): Unit = resource.close()
+  implicit val releaseSocket: Releasable[ServerSocket] = _.close()
+
+  def bind(address: InetSocketAddress) = {
+    val serverSocket = new ServerSocket()
+    try {
+      serverSocket.bind(address.asJava)
+      Right(serverSocket)
+    } catch {
+      case e: IOException =>
+        serverSocket.close()
+        Left(BindError.AlreadyBound(e))
+      case e: SecurityException =>
+        serverSocket.close()
+        Left(BindError.SecurityError(e))
+    }
+  }
 }
-
-private def bind(address: InetSocketAddress) =
-  val serverSocket = ServerSocket()
-  try
-    serverSocket.bind(address.asJava)
-    Right(serverSocket)
-  catch
-    case e: IOException =>
-      serverSocket.close()
-      Left(BindError.AlreadyBound(e))
-    case e: SecurityException =>
-      serverSocket.close()
-      Left(BindError.SecurityError(e))
 
 object Server {
   def main(args: Array[String]): Unit = {
     val address = InetSocketAddress(Host("localhost"), Port(6379))
-    bind(address) match
+    bind(address) match {
       case Left(error) =>
         Console.err.println(s"Failed to bind to $address: $error")
         System.exit(1)
@@ -66,7 +61,7 @@ object Server {
           println(s"Server is running on ${serverSocket.getLocalSocketAddress}")
           val socket = serverSocket.accept()
           println(s"Accepted connection from ${socket.getInetAddress}")
-          Thread.sleep(10000)
         }
+    }
   }
 }
