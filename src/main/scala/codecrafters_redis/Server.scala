5faf84df92181ff1158f7fa767520768da987905
NavidJalali <navid.jalali@moia.io>
2024-11-30 03:52:02 +0100

    uncomment server

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -6,11 +6,10 @@ object Server {
   def main(args: Array[String]): Unit = {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println("Logs from your program will appear here!")
-
-    // Uncomment this to pass the first stage
-    //
-    // val serverSocket = new ServerSocket()
-    // serverSocket.bind(new InetSocketAddress("localhost", 6379))
-    // val clientSocket = serverSocket.accept() // wait for client
+    val serverSocket = new ServerSocket()
+    serverSocket.bind(new InetSocketAddress("localhost", 6379))
+    val clientSocket = serverSocket.accept() // wait for client
+    clientSocket.close()
+    serverSocket.close()
   }
 }
